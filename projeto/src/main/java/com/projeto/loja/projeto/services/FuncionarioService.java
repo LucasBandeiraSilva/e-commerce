84625c7807b8e3e2645f77a68745166e427a34ff
brunosouza2 <bruno.henrqsouza@gmail.com>
2024-04-24 20:00:48 -0300

    renomeando service de funcionário

--- projeto/src/main/java/com/projeto/loja/projeto/services/FuncionarioService.java
+++ projeto/src/main/java/com/projeto/loja/projeto/services/FuncionarioService.java
@@ -3,7 +3,6 @@ package com.projeto.loja.projeto.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,7 @@ import com.projeto.loja.projeto.model.Funcionario;
 import com.projeto.loja.projeto.repositories.FuncionarioRepository;
 
 @Service
-public class funcionarioService {
+public class FuncionarioService {
 
 
     @Autowired
