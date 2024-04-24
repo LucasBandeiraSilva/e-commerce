4caed16897d475c2ac291c3234ef7a35737a4fe1
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-04-24 16:36:10 -0300

    reformulando mensagem de cpf

--- projeto/src/main/java/com/projeto/loja/projeto/dto/FuncionarioDto.java
+++ projeto/src/main/java/com/projeto/loja/projeto/dto/FuncionarioDto.java
@@ -16,7 +16,7 @@ public record FuncionarioDto(
 
         @Size(max = 150, message = "e-mail must be shorter") @Email(message = "email invalid") String email,
 
-        @CPF(message = "Formato de cpf inválido.") @Column(unique = true) String cpf,
+        @CPF(message = "Invalid cpf format.") @Column(unique = true) String cpf,
 
         @Size(min = 6, message = "the password must be longer than 6 characters") @Size(max = 50, message = "the password must be shorter than 50 characters") String senha,
 
